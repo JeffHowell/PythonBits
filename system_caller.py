744bea153a232d6e6759c1abe128b9615ca8e5cf
JeffHowell <jeffrey.howell@gmail.com>
2016-11-05 00:32:19 -0400

    Working on subprocess.Popen version, currently failing

--- system_caller.py
+++ system_caller.py
@@ -11,6 +11,10 @@ class SystemCaller:
     def os_system_call(self, command):
         return os.system(command)
 
+    def os_popen_call(self, command):
+        proc = subprocess.Popen(shell=True)
+        proc.communicate()
+
     def subprocess_call(self, command):
         subprocess.Popen(command)
         return "foo"
--- test_system_call.py
+++ test_system_call.py
@@ -22,8 +22,11 @@ class MyTestCase(unittest.TestCase):
     def test_unsuccessful_return_code_os_system(self):
         self.assertNotEqual(0, self.uut.os_system_call("false"))
 
-    def test_successful_return_stdout(self):
-        self.assertEqual("foo", self.uut.subprocess_call("echo foo", shell=True, stdout=self.uut.subprocess.PIPE).stdout.read())
+    # def test_successful_return_stdout(self):
+    #     self.assertEqual("foo", self.uut.subprocess_call("echo foo", shell=True, stdout=self.uut.subprocess.PIPE).stdout.read())
+
+    def test_successful_os_popen(self):
+        self.uut.os_popen_call()
 
 if __name__ == '__main__':
     unittest.main()
